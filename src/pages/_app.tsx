966e00a91aab0ebfcf606b8ef0b35418ea8e7db9
Arjun <arjun140702@gmail.com>
2022-10-10 03:39:08 +0530

    Setup theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+
+const theme = extendTheme({
+    initialColorMode: "dark",
+    useSystemColorMode: true
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
             <Component {...pageProps} />
         </ChakraProvider>
     )
