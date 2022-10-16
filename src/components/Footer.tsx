ac594b89a45405fcb9f1e48ab12db446c667ba91
Arjun <arjun140702@gmail.com>
2022-10-17 02:44:39 +0530

    Add links, make footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,37 +1,49 @@
-import { Flex, Link, Stack, Text } from "@chakra-ui/react"
+import { Flex, Icon, Link, Spacer, Stack, Text } from "@chakra-ui/react"
 
 const Footer = () => {
     return (
-        <Stack>
-            <Flex
-                pt={3}
-                flexWrap="wrap"
-                flexDirection="row"
-                gap={1}
-                justify="center"
-                fontSize="sm">
-                <Text>
-                    &copy; 2022{" "}
-                    <Link href="https://imarjun.me" textDecoration="underline">
-                        Arjun Sivaraman
-                    </Link>{" "}
-                </Text>
-                <Text>
-                    Powered by{" "}
-                    <Link
-                        href="https://nextjs.org"
-                        textDecoration="underline"
-                        target="_blank">
-                        Next.js
-                    </Link>{" "}
-                    and{" "}
-                    <Link
-                        href="https://chakra-ui.com"
-                        textDecoration="underline"
-                        target="_blank">
-                        Chakra UI
-                    </Link>
-                </Text>
+        <Stack
+            direction={{ base: "column", md: "row" }}
+            align="center"
+            spacing={{ base: 2, md: 4 }}
+            w="100%"
+            px={8}
+            fontSize="sm">
+            <Text flexShrink={0} textAlign={{ base: "center", md: "end" }}>
+                &copy; 2022{" "}
+                <Link href="https://imarjun.me" textDecoration="underline">
+                    Arjun Sivaraman
+                </Link>{" "}
+            </Text>
+            <Spacer />
+            <Text textAlign="center">
+                Powered by{" "}
+                <Link
+                    href="https://nextjs.org"
+                    textDecoration="underline"
+                    target="_blank">
+                    Next.js
+                </Link>{" "}
+                and{" "}
+                <Link
+                    href="https://chakra-ui.com"
+                    textDecoration="underline"
+                    target="_blank">
+                    Chakra UI
+                </Link>
+            </Text>
+            <Spacer />
+            <Flex gap={{ base: 2, md: 4 }} flexWrap="wrap">
+                <Link
+                    href="https://github.com/1407arjun/vit-free-slot-finder"
+                    textDecoration="underline">
+                    GitHub
+                </Link>
+                <Link
+                    href="https://showcase.imarjun.me"
+                    textDecoration="underline">
+                    Showcase
+                </Link>
             </Flex>
         </Stack>
     )
