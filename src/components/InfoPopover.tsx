3a9bb996e0e261fa42e609bf0a048b544e518cde
Arjun <arjun140702@gmail.com>
2022-10-17 15:50:39 +0530

    Update popover info

--- src/components/InfoPopover.tsx
+++ src/components/InfoPopover.tsx
@@ -6,10 +6,9 @@ import {
     PopoverArrow,
     PopoverCloseButton,
     PopoverHeader,
-    PopoverBody,
-    ListItem
+    PopoverBody
 } from "@chakra-ui/react"
-import { IconButton, UnorderedList } from "@chakra-ui/react"
+import { ListItem, IconButton, UnorderedList, Text } from "@chakra-ui/react"
 
 const InfoPopover = () => {
     return (
@@ -29,9 +28,28 @@ const InfoPopover = () => {
                 </PopoverHeader>
                 <PopoverBody>
                     The CSV file should contain at least the following columns:
-                    <UnorderedList>
+                    <UnorderedList mb={4}>
                         <ListItem fontWeight="semibold">Name</ListItem>
                         <ListItem fontWeight="semibold">Timetable</ListItem>
+                    </UnorderedList>
+                    For the timetable entries follow the steps below:
+                    <UnorderedList>
+                        <ListItem>
+                            Go to your timetable on VTOP. Scroll down to see the
+                            table with your schedule
+                        </ListItem>
+                        <ListItem>
+                            Select the text from{" "}
+                            <Text fontWeight="semibold" display="inline">
+                                THEORY
+                            </Text>{" "}
+                            in the top left to{" "}
+                            <Text fontWeight="semibold" display="inline">
+                                L94 -
+                            </Text>{" "}
+                            in the bottom right
+                        </ListItem>
+                        <ListItem>Copy-paste all of the selected text</ListItem>
                     </UnorderedList>
                 </PopoverBody>
             </PopoverContent>
