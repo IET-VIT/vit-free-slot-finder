bb0d9dd063721485bd08c3ecd33b87bd1ad7acbb
Arjun <arjun140702@gmail.com>
2022-12-12 16:53:32 +0530

    Fix missing imports

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,5 @@
 import { ColorModeScript } from "@chakra-ui/react"
 import NextDocument, { Html, Head, Main, NextScript } from "next/document"
-import { theme } from "./_app"
 
 export default class Document extends NextDocument {
     render() {
